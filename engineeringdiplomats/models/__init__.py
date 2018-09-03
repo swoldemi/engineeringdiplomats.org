3b6dec68848bba6cfe3bf788c81f8a325ee0a33e
Simon W <b30bd351371c686298d32281b337e8@gmail.com>
2018-09-03 02:57:57 -0500

    Add QuestionDocument model

--- engineeringdiplomats/models/__init__.py
+++ engineeringdiplomats/models/__init__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
 
-from .db import MongoConnector
+from .db import MongoConnector, QuestionDocument
 from .forms import StudentQueryForm
 from .user import User
--- engineeringdiplomats/models/db.py
+++ engineeringdiplomats/models/db.py
@@ -10,6 +10,29 @@ from pymongo import MongoClient
 from pymongo.errors import ExecutionTimeout, OperationFailure, ServerSelectionTimeoutError
 
 
+class QuestionDocument(dict):
+	"""Encapsulates the structure of a question to be submitted 
+	into the questions collection.
+	
+	Inherits
+	--------
+	Type : dict
+
+	Attributes
+	-----------
+	question_id : UUID
+		Unique identifier for retriving the question.
+	submitters_name : str
+		The name of the student that submitted the question.
+	submitters_email : str
+		The email of the student that submitted the question.
+	question : str
+		The question that the student submitted.
+	"""
+	def __init__(self, *args, **kwargs):
+		super(QuestionDocument, self).__init__(*args, **kwargs)
+
+
 class MongoConnector(object):
 	"""Encapsulates a connection to MongoDB.
 
