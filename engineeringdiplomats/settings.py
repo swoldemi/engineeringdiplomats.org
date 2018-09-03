8a4d1533f28bb4f1b11ffa9db302c769de41db82
Simon W <b30bd351371c686298d32281b337e8@gmail.com>
2018-09-03 04:50:51 -0500

    Add package location and file object for logging emails

--- engineeringdiplomats/settings.py
+++ engineeringdiplomats/settings.py
@@ -4,9 +4,11 @@
 
 import os
 
+__location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
+
 microsoft_oauth_config = {
-    "consumer_key" : os.environ["AZURE_ID"],
-    "consumer_secret" : os.environ["AZURE_KEY"],
+    "consumer_key" : os.environ.get("AZURE_ID"),
+    "consumer_secret" : os.environ.get("AZURE_KEY"),
     "request_token_params" : {"scope": "User.Read"},
     "base_url" : "https://graph.microsoft.com/v1.0/",
     "request_token_url" : None,
@@ -14,3 +16,5 @@ microsoft_oauth_config = {
     "access_token_url" : "https://login.microsoftonline.com/organizations/oauth2/v2.0/token",
     "authorize_url" : "https://login.microsoftonline.com/organizations/oauth2/v2.0/authorize"
 }
+
+emails_log_file = open(os.path.join(__location__, "../logs/emails.log"), "a+")
