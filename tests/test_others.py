649fd484ac7ecc9c1d6216eda5743d4911bc1667
Simon W <b30bd351371c686298d32281b337e8@gmail.com>
2018-09-14 21:17:39 -0500

    Add tests for utility functions and decorators

---
+++ tests/test_others.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+
+"""Tests for helper functions and specific decorators."""
+
+import threading
+
+from time import sleep
+
+from engineering_diplomats.utilities import get_events, send_text_message
+
+import pytest
+
+class TestSuiteOther(object):
+
+    def test_send_text(self):
+        """Send a text message. Sleeps current thread until
+        the task thread used to send the text has died.
+        """
+        assert send_text_message("Hi") is None
+        is_alive = False
+        task_thread = None
+        for thread in threading.enumerate():
+            if thread.name == "send_text_message":
+                task_thread = thread
+                break
+        while task_thread.is_alive():
+            sleep(1)
+
+        assert not task_thread.is_alive()
+
+
+    def test_get_events(self):
+        """Get all events from 
+        ttuengineeringdiplomats@gmail.com's Google Calendar."""
+        all_events = get_events()
+        for event in all_events.keys():
+            print(event)
+        
