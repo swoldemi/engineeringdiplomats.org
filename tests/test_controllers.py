50df930f173cf157513257924f039318ae407961
Simon W <b30bd351371c686298d32281b337e8@gmail.com>
2018-09-23 22:19:19 -0500

    Add base tests for Mailer

--- tests/test_controllers.py
+++ tests/test_controllers.py
@@ -6,6 +6,7 @@ from uuid import uuid4
 import pytest
 
 from bson.objectid import ObjectId
+from flask_mail import Mail
 
 from engineering_diplomats.controllers import MongoConnector, Mailer
 from engineering_diplomats.models import QuestionDocument
@@ -19,8 +20,7 @@ class TestSuiteControllers(object):
         Parameters
         ----------
         app : flask.Flask
-            Instance of the application injected as a test fixture
-            by pytest.
+            Instance of the application injected by pytest.
         """
         mongo_connector = MongoConnector(app)
 
@@ -46,6 +46,29 @@ class TestSuiteControllers(object):
                 found = True
         assert found
 
-
         # Delete the question that was just added
         assert mongo_connector.remove_question(question_doc["question_id"])
+
+
+    def test_mailer(self, app):
+        """Test mailing controller.
+
+        Parameters
+        ----------
+        app : flask.Flask
+            Instance of the application injected by pytest.
+        """
+        mailer = Mailer(Mail(app))
+        
+        question_doc = QuestionDocument(
+            question_id=uuid4().hex,
+            submitters_name="Simon Woldemichael",
+            submitters_email="simon.woldemichael@ttu.edu",
+            submission_date=datetime.now(),
+            question="How do I study abroad?",
+        )
+
+        assert mailer.send_notification(question_doc) is None
+        assert mailer.send_confirmation(question_doc) is None
+        assert mailer.send_answer("Answer", question_doc) is None
+            
