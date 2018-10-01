02b2c9e4a0e90e6a894ef427dc5b8e513ff49962
Simon W <b30bd351371c686298d32281b337e8@gmail.com>
2018-10-01 00:45:42 -0500

    Add jQuery code to expand answer submissions
    Add fade for flashed messages

--- engineering_diplomats/views/static/js/main.js
+++ engineering_diplomats/views/static/js/main.js
@@ -1,9 +1,29 @@
 // Hamburger menu for mobile
 (function () {
-    var burger = document.querySelector('.burger');
-    var menu = document.querySelector('#' + burger.dataset.target);
-    burger.addEventListener('click', function () {
-        burger.classList.toggle('is-active');
-        menu.classList.toggle('is-active');
+    var burger = document.querySelector(".burger");
+    var menu = $("#" + burger.dataset.target);
+    burger.addEventListener("click", function () {
+        if (menu.is(":visible")) {
+            menu.hide(100);
+        } else {
+            menu.show(100);
+        }
+        burger.classList.toggle("is-active");
+        menu.toggleClass("is-active");
     });
+
+    // Expander for answer submissions
+    var answer_expander = $(".answer-expander");
+    answer_expander.click(function () {
+        var answer_container = $($(this).parent().get(0)).find("form");
+        if (answer_container.is(":visible")) {
+            answer_container.hide(100);
+        } else {
+            answer_container.show(100);
+            answer_container.css("display", "grid");
+        }
+    });
+
+    // Flash message fade
+    $(".flash").delay(2000).slideUp(1500);
 })();
