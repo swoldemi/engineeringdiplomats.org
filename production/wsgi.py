a77a0550953c70f8f0468b63b5fcfc5886a4e877
Simon W <b30bd351371c686298d32281b337e8@gmail.com>
2018-10-09 04:42:31 -0500

    Add initial WSGI entrypoint

--- production/wsgi.py
+++ production/wsgi.py
@@ -1,15 +1,30 @@
 # -*- coding: utf-8 -*-
 
-import logging
 import os
 import sys
 
+import logme
+
 from gevent.pywsgi import WSGIServer
 
+from engineering_diplomats.main import init_app
 
-def main():
-    pass
 
+@logme.log
+def main(logger=None) -> None:
+	"""Main routine to initialize and start the production environment."""
+	app = init_app()
+	os.environ["FLASK_ENV"] = "production"
+	os.environ["DEBUG"] = "0"
+	listener = ("0.0.0.0", 80)
+
+	try:
+		WSGIServer(listener=listener, application=app, log=app.logger).serve_forever()
+	except OSError as e:
+		logger.exception(e)
+		raise
+	except Exception as e:
+		logger.exception(e)
 
 if __name__ == "__main__":
-    main()
+	main()
