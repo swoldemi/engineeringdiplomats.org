21e28092d0e8e9df1e11a082fe3d74f17021e349
Simon W <b30bd351371c686298d32281b337e8@gmail.com>
2018-10-11 00:31:53 -0500

    Run application on :8080
    Accounts for NGINX reverse proxy setup

--- production/wsgi.py
+++ production/wsgi.py
@@ -13,13 +13,19 @@ from engineering_diplomats.main import init_app
 @logme.log
 def main(logger=None) -> None:
 	"""Main routine to initialize and start the production environment."""
+	
 	app = init_app()
 	os.environ["FLASK_ENV"] = "production"
 	os.environ["DEBUG"] = "0"
-	listener = ("0.0.0.0", 80)
-
+	here = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
+	logger.info(f"Running from {here}")
+	
 	try:
-		WSGIServer(listener=listener, application=app, log=app.logger).serve_forever()
+		WSGIServer(
+			listener=("0.0.0.0", 8080), 
+			application=app,  
+			log=logger,
+		).serve_forever()
 	except OSError as e:
 		logger.exception(e)
 		raise
