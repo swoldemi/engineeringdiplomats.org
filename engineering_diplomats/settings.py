3c2d7018d031a76da73b474e01a5027df6fff71a
Simon W <b30bd351371c686298d32281b337e8@gmail.com>
2018-10-12 02:42:50 -0500

    Add Instagram link to social buttons in footer
    Small adjustment to formatting

--- engineering_diplomats/settings.py
+++ engineering_diplomats/settings.py
@@ -14,7 +14,7 @@ microsoft_oauth_config = {
     "request_token_url" : None,
     "access_token_method" : "POST",
     "access_token_url" : "https://login.microsoftonline.com/organizations/oauth2/v2.0/token",
-    "authorize_url" : "https://login.microsoftonline.com/organizations/oauth2/v2.0/authorize"
+    "authorize_url" : "https://login.microsoftonline.com/organizations/oauth2/v2.0/authorize",
 }
 
 app_config_kwargs = {
