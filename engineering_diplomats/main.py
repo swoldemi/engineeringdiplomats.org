b909777048654c4ed5926e058775a699fcfe678c
Simon W <b30bd351371c686298d32281b337e8@gmail.com>
2018-12-02 03:06:00 -0600

    Implement tracer using GCP Stackdriver Trace

--- engineering_diplomats/main.py
+++ engineering_diplomats/main.py
@@ -9,6 +9,11 @@ from flask import Flask
 from flask_mail import Mail
 from flask_oauthlib.client import OAuth
 
+from opencensus.trace.exporters import stackdriver_exporter
+from opencensus.trace.ext.flask.flask_middleware import FlaskMiddleware as Tracing
+from opencensus.trace.exporters.transports.background_thread \
+    import BackgroundThreadTransport
+
 from engineering_diplomats.controllers import Mailer, MongoConnector
 from engineering_diplomats.routes import apply_routes
 from engineering_diplomats.settings import microsoft_oauth_config, app_config_kwargs
@@ -31,7 +36,12 @@ def init_app(logger=None) -> Flask:
 	oauth = OAuth(app)
 	db = MongoConnector(app)
 	mailer = Mailer(Mail(app))
-	
+
+	exporter = stackdriver_exporter.StackdriverExporter(
+    	project_id=os.environ.get("GCP_PROJECT"), transport=BackgroundThreadTransport
+	)
+	tracer = Tracing(app, exporter=exporter)
+
 	microsoft = oauth.remote_app("microsoft", **microsoft_oauth_config)
 	site_handler = SiteHandler(db, microsoft, mailer)
 	site_handler.get_token = microsoft.tokengetter(site_handler.get_token)
