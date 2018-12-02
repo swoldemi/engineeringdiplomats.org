a92c5245fd447932359e8b092cf9f6339ee51234
Simon W <b30bd351371c686298d32281b337e8@gmail.com>
2018-12-02 06:01:50 -0600

    Tag builds using commit that triggered build
    You should avoid using the :latest tag when deploying containers in production as it is harder to track which version of the image is running and more difficult to roll back properly.

--- k8s/deploy.sh
+++ k8s/deploy.sh
@@ -8,10 +8,10 @@ gcloud --quiet config set project $GCP_PROJECT_ID
 gcloud --quiet config set compute/zone $COMPUTE_ZONE
 
 # Build new image and push to private registry
-docker build -t gcr.io/$GCP_PROJECT_ID/engineeringdiplomats.org:latest .
+docker build -t gcr.io/$GCP_PROJECT_ID/engineeringdiplomats.org:$TRAVIS_COMMIT .
 docker login -u _json_key -p "$(cat gcp-creds.json)" https://gcr.io
-docker push gcr.io/$GCP_PROJECT_ID/engineeringdiplomats.org:latest
+docker push gcr.io/$GCP_PROJECT_ID/engineeringdiplomats.org:$TRAVIS_COMMIT
 
 # Update all pods on cluster
 gcloud container clusters get-credentials ed-cluster-1 --zone $COMPUTE_ZONE --project $GCP_PROJECT_ID
-kubectl set image deployments/engineeringdiplomats engineeringdiplomats=gcr.io/$GCP_PROJECT_ID/engineeringdiplomats.org:latest
+kubectl set image deployments/engineeringdiplomats engineeringdiplomats=gcr.io/$GCP_PROJECT_ID/engineeringdiplomats.org:$TRAVIS_COMMIT
