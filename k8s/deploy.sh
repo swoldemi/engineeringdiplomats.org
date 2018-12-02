cc3730afc3a425d04ed207754ead9ed3d14314db
Simon W <b30bd351371c686298d32281b337e8@gmail.com>
2018-12-02 05:26:16 -0600

    Update deploy provider

--- k8s/deploy.sh
+++ k8s/deploy.sh
@@ -7,6 +7,10 @@ gcloud auth activate-service-account --key-file=gcp-creds.json
 gcloud --quiet config set project $GCP_PROJECT_ID
 gcloud --quiet config set compute/zone $COMPUTE_ZONE
 
+# Build new image and push to private registry
 docker build -t gcr.io/$GCP_PROJECT_ID/engineeringdiplomats.org:latest .
 docker login -u _json_key -p "$(cat gcp-creds.json)" https://gcr.io
 docker push gcr.io/$GCP_PROJECT_ID/engineeringdiplomats.org:latest
+
+# Update all pods on cluster
+kubectl set image deployments/engineeringdiplomats engineeringdiplomats=gcr.io/$GCP_PROJECT_ID/engineeringdiplomats.org:latest
